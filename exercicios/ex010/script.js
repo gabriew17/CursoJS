93112a1ebc9022520069c786fbc4d1a706c766ef
gabriew17 <gabriewantony@gmail.com>
2026-02-24 22:07:57 -0300

    add ex010

---
+++ exercicios/ex010/script.js
@@ -0,0 +1,16 @@
+function Clicar1(){
+    const res = document.getElementById("res")
+    res.innerHTML += "<p>Clicou no primeiro botão</p>"
+}
+function Clicar2(){
+    const res = document.getElementById("res")
+    res.innerHTML += "<p>Clicou no segundo botão</p>"
+}
+function Clicar3(){
+    const res = document.getElementById("res")
+    res.innerHTML += "<p>Clicou no terceiro botão</p>"
+}
+function Clicar4(){
+    const res = document.getElementById("res")
+    res.innerHTML += "<p>Clicou no quarto botão</p>"
+}
