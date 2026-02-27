7550499e7aa46faadc2858369fb118da652d4d2f
gabriew17 <gabriewantony@gmail.com>
2026-02-27 19:07:56 -0300

    add aula011

---
+++ aulas/aula011/ex009.js
@@ -0,0 +1,8 @@
+var país = "EUA"
+console.log(`Vivendo em ${país}`)
+// Estrutura condicional para verificar o país.
+if (país == "Brasil"){
+    console.log("Você é Brasileiro")
+}else {
+    console.log("Você é estrangeiro")
+}
